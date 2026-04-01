1fd8288e6ab55e1f749edd40284e908a3a3c2050
Asokan k <asokansrm03@gmail.com>
2026-04-01 20:47:56 +0530

    Create decoder2_4.v

---
+++ Decoder/decoder2_4.v
@@ -0,0 +1,19 @@
+module decoder(
+  input [1:0]i,
+  input en,
+  output reg [3:0]y
+);
+
+  always @(*) begin
+    if(en) begin
+      case(i)
+        2'b00: y = 4'b0001;
+        2'b01: y = 4'b0010;
+        2'b10: y = 4'b0100;
+        2'b11: y = 4'b1000;
+      endcase
+    end
+    else
+      y = 4'b0;
+  end
+endmodule
