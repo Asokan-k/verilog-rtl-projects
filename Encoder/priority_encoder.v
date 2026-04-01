d56ea1e5fe22295ff37ec48addf0e02f9de1a43f
Asokan k <asokansrm03@gmail.com>
2026-04-01 21:07:54 +0530

    Create priority_encoder.v

---
+++ Encoder/priority_encoder.v
@@ -0,0 +1,23 @@
+module priority_encoder_4to2 (
+    input  [3:0] i,
+    output reg [1:0] y,
+    output reg valid
+);
+
+always @(*) begin
+    valid = 1'b1;
+
+    casex (i)
+        4'b1xxx: y = 2'b11;
+        4'b01xx: y = 2'b10;
+        4'b001x: y = 2'b01;
+        4'b0001: y = 2'b00;
+        default: begin
+            y = 2'b00;
+            valid = 1'b0;
+        end
+    endcase
+
+end
+
+endmodule
