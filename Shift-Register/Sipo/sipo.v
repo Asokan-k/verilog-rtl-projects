e0cba9907405cd9c9be1fa6bfa1f876b9a4019f5
Asokan k <asokansrm03@gmail.com>
2026-04-03 09:58:36 +0530

    Create sipo.v

---
+++ Shift-Register/Sipo/sipo.v
@@ -0,0 +1,15 @@
+module sipo (
+    input  wire clk,
+    input  wire rst,
+    input  wire din,
+    output reg [3:0] q
+);
+
+always @(posedge clk or posedge rst) begin
+    if (rst)
+        q <= 4'b0000;
+    else
+        q <= {q[2:0], din};
+end
+
+endmodule
